92033dac80efe992d1ad599f6fd04aa4b1e1cd83
brianrossegan@gmail.com <brianrossegan@gmail.com@0ee034da-9f2d-11de-b2a8-a7b03212e241>
2009-09-23 19:03:17 +0000

    Adding the item_viewer and pan_view.php code to the trunk like I should have done from the beginngin. Forgive my SVN nubbery!

---
+++ item_viewer.php
@@ -0,0 +1,4 @@
+<!-- This File contains the PHP & HTML you will need to place into cdm4/item_viewer.php -->
+
+<!-- Please place the following in the <head> of your document -->
+<link rel="stylesheet" href="/dmMonocle-1.0/styles/dmmonocle.css" type="text/css" />
