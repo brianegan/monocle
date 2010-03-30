7ae14a7281cb7e10d29207406e76cfb947e5f732
brianegan <brian.egan@unlv.edu>
2010-03-30 00:00:52 -0700

    Setting up the tests

--- spec/unit/spec.js
+++ spec/unit/spec.js
@@ -1,8 +1,22 @@
-
-describe 'YourLib'
-  describe '.someMethod()'
-    it 'should do something'
-      true.should.be true
+describe 'dmMonocle'
+    
+    before_each
+        $(fixture('monocle')).append('body')
     end
-  end
+    
+    after_each
+      $('#dmMonocle').remove()
+    end    
+    
+    describe 'Init'
+        it 'should calculate the size of the viewable area'
+            
+        end
+    end
+    
+    describe ''
+      
+    end
+    
+    
 end
