64a82865c753db26e6d23232ef320aa7f4f3d484
Brian Egan <brian@brianegan.com>
2010-10-13 16:43:14 -0700

    Initial Tests -- Basic router and init method

--- spec/spec.monocle.js
+++ spec/spec.monocle.js
@@ -11,12 +11,24 @@ describe("monocle", function() {
 
   // The plugin is essentially a router to call the appropriate
   // methods. See the official jQuery docs for info.
-  describe("plugin router", function() {
-    
-    it("should fire the constructor if no options passed", function() {
-      monocle.monocle();
-      expect(monocle.monocle('init')).toHaveBeenCalled();
+  describe("plugin", function() {
+
+    it("should define monocle jquery plugin", function() {
+      expect($.fn.monocle).toBeDefined();
     });
+    
+    it("should fire the constructor by default", function() {
+      spyOn(Monocle, 'init');
+      monocle.monocle();
+      expect(Monocle.init).toHaveBeenCalled();
+    });
+
+    it("should fire the proper method", function() {
+      spyOn(Monocle, 'destroy');
+      monocle.monocle('destroy');
+      expect(Monocle.destroy).toHaveBeenCalled();      
+    });
+    
 
   });  
  
@@ -27,7 +39,7 @@ describe("monocle", function() {
       monocle.monocle();
     });
 
-    it("should attach data to the element", function() {
+    it("should attach data to the element if none exists", function() {
       expect(monocle.data('monocle').target).toEqual(monocle);
     });     
 
