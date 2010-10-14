0ff3aed2b0ae867e907f1f84c95964aa38fd7446
Brian Egan <brian@brianegan.com>
2010-10-14 10:58:16 -0700

    Redoing many of the calculators in a new way using jQuery data rather than a dumbass object

--- spec/spec.monocle.js
+++ spec/spec.monocle.js
@@ -1,12 +1,29 @@
+/*
+ * Monocle Tests ====================================================================
+ *
+ * BDD Tests are so sexy. Learn more by Googlin' JasmineBDD
+ */
+
 describe("monocle", function() {
   
-  // Setup and Teardown functions 
+  // Create a sandbox div
   beforeEach(function() {
-    monocle = $('<div id="monocle" />');
+    monocle = $('<div />', {
+      id: 'sandbox',
+      css: {
+        width: '900px',
+        height: '400px',
+        padding: '50px'
+      }
+    });
+
+    monocle.appendTo('body');
   });
 
+  // Reset monocle so it has completely fresh state
   afterEach(function() { 
-    monocle = {}; 
+    $('body').remove(monocle);
+    monocle = {};
   });
 
   // The plugin is essentially a router to call the appropriate
@@ -15,6 +32,10 @@ describe("monocle", function() {
 
     it("should define monocle jquery plugin", function() {
       expect($.fn.monocle).toBeDefined();
+    });
+
+    it("it should have default options", function() {
+      expect($.fn.monocle.defaults).toBeDefined();
     });
     
     it("should fire the constructor by default", function() {
@@ -41,6 +62,7 @@ describe("monocle", function() {
       } catch (e) {
         expect(e).toEqual('Method zombies does not exist on jQuery.monocle');
       }
+      // expect(monocle.monocle('zombies')).toThrow('Method zombies does not exist on jQuery.monocle'); 
     });
     
   });  
@@ -49,16 +71,85 @@ describe("monocle", function() {
   describe("init", function() {
 
     beforeEach(function() {
-      monocle.monocle();
+      monocle.monocle({ 
+        CISOROOT: '/zombies',  
+        CISOPTR: 420, 
+        dmImageHeight: 5000, 
+        dmImageWidth: 10000, 
+        dmBridge: false,
+        initRotation: 90,
+        searchText: "snow",
+        urlPrefix: "/" 
+      });
+      data = monocle.data('monocle');
     });
 
     it("should attach data to the element if none exists", function() {
-      expect(monocle.data('monocle').target).toEqual(monocle);
-    });     
+      expect(data).not.toBeNull();
+    });
 
+    it("should attach the options to the dom element", function() {
+       expect(data.options).not.toBeNull();
+    });
+
+    it("should define the rotation", function() {
+      expect(data.rotation).toEqual(90);
+    });
+
+    describe("Viewer Calculations", function() {
+      it("should calculate & store the width of the viewer, including padding", function() {
+        expect(data.viewer.width).toEqual(1000);
+      });
+      
+      it("should calculate & store the height of the viewer, including padding", function() {
+         expect(data.viewer.height).toEqual(500);
+      });
+    });  
+
+    describe("Image Calculations", function() {
+      it("should store the width of the image", function() {
+        expect(data.dmImage.width).toEqual(10000);
+      });
+      
+      it("should store the height of the image", function() {
+        expect(data.dmImage.height).toEqual(5000);
+      });
+
+      it("should store the rotated width of the image", function() {
+        expect(data.dmImage.rotatedWidth).toEqual(5000);
+      });
+      
+      it("should store the rotated height of the image", function() {
+        expect(data.dmImage.rotatedHeight).toEqual(10000);
+      });
+    });  
+    
+    describe("minimum zoom properties", function() {
+      it("should store the minimum zoom level", function() {
+        expect(data.minZoom).toEqual(0.1);
+      });
+      
+      it("should store the mimimum zoom width", function() {
+        expect(data.minZoomWidth).toEqual(500); 
+      });
+
+      it("should store the minimum zoom height", function() {
+        expect(data.minZoomHeight).toEqual(1000);
+      });
+    });  
+    
+        
+    describe("zoom Image", function() {
+      it("should create a zoom image", function() {
+         expect(data.zoomImage).toBeDefined();
+      });
+    });  
+    
+    
+
+    
+    
   });
-
-  
 
 });  
 
--- src/monocle.js
+++ src/monocle.js
@@ -4,50 +4,192 @@
  * This file contains all the core methods that work for all types of monocles
  */
 
-// Stop JavaScript Lint Warnings Messages
+// Stop Annoying JavaScript Lint Warnings Messages
 /*jsl:ignoreall*/
   
 (function($) {
 
   /*
-   * Private Helper Functions
+   * Calculators ====================================================================
+   *
+   * We need to make a number of calculations at various points in the program, so
+   * I've tried to abstract the most important calculations into a number of helper
+   * functions found below.
+   */
+    
+  /**
+   * Calculates the Dom Element Dimensions
+   * @private
+   * @returns width or height 
+   * @type Number
    */
   
+  function calcViewerDims(ret, el) {
+    switch (ret) {
+      case 'width':
+        return el.innerWidth();
+        break;
+      case 'height':
+        return el.innerHeight();
+        break;
+      case 'default':
+        return 
+        break;
+    }
+  }
+  
+  /**
+   * Calculates the dimensions of the image based on rotation
+   * @private
+   * @returns width or height
+   * @type Number
+   */	
+  function calcDmImageRotatedDimensions(ret, el) {		
+    var rotatedWidth, 
+        rotatedHeight,
+        data = el.data('monocle');
+    
+    if (data.rotation === 0 || data.rotation === 180) {
+      rotatedWidth = data.options.dmImageWidth;
+      rotatedHeight = data.options.dmImageHeight;
+    }	else {
+      rotatedWidth = data.options.dmImageHeight;
+      rotatedHeight = data.options.dmImageWidth;
+    }
+    
+    switch (ret) {
+      case 'width':
+        return rotatedWidth;
+        break;
+      case 'height':
+        return rotatedHeight; 
+        break;
+      case 'default':
+        return 
+        break;
+    }
+  };
+  
+  /**
+   * Calculate the minimum zoom level based on the dimensions of image
+   * @private
+   * @returns minimum zoom level in decimal form
+   * @type Number
+   */
+  function calcMinZoom(el) {		
+    var minZoomWidthRatio, 
+        minZoomHeightRatio,
+        data = el.data('monocle');
+    
+    if (data.rotation === 0 || 180) {
+      minZoomWidthRatio = data.viewer.width / data.dmImage.width;
+      minZoomHeightRatio = data.viewer.height / data.dmImage.height;
+    }	else {
+      minZoomWidthRatio = data.viewer.width / data.dmImage.height;
+      minZoomHeightRatio = data.viewer.height / data.dmImage.width;
+    }
+    return minZoomWidthRatio >= minZoomHeightRatio 
+           ? minZoomHeightRatio
+           : minZoomWidthRatio;
+  };
+
+  /**
+   * Calculates the dimensions of a given image at a certain rotation and zoom level
+   * can be used for our main image as well as the zoomImage
+   * @private
+   * @returns { width, height }
+   * @type Object
+   */		
+  function calcImageDimensions(ret, zoom, el) {
+    var data = el.data('monocle');
+
+    switch (ret) {
+      case 'width':
+        return data.dmImage.rotatedWidth * zoom;
+        break;
+      case 'height':
+        return data.dmImage.rotatedHeight * zoom; 
+        break;
+      case 'default':
+        return 
+        break;
+    }
+  };
+
   /*
    * Monocle Methods 
    */
-  
   Monocle = {
-     init: function(options) {
+    init: function(options) {
 
-       return this.each(function(){
+      return this.each(function(){
+
+        var $this = $(this),
+            data = $this.data('monocle'),
+            opts = $.extend({},	$.fn.monocle.defaults, options);
          
-         var $this = $(this),
-             data = $this.data('monocle');
-         
-         // If the plugin hasn't been initialized yet
-         if (!data) {
-           
-           $(this).data('monocle', {
-               target : $this
-           });
-         }
-       });
-     },
-     destroy : function( ) {
+        // If the plugin hasn't been initialized yet, a bunch of calculations
+        // need to occur and be saved to the element's data, then the 
+        // various constructors need to be called
+        if (!data) {
 
-       return this.each(function(){
+          // We're not loading in everything into the data object 
+          // at once because some methods depend on data being available.
+          $this.data('monocle', {
+            target : $this,
+            options: opts
+          });
 
-         var $this = $(this),
-             data = $this.data('monocle');
+          // Cache the data object now that it exists
+          data = $this.data('monocle');
 
-         // Namespacing FTW
-         $(window).unbind('.monocle');
-         data.monocle.remove();
-         $this.removeData('monocle');
+          data.rotation = data.options.initRotation
+                          ? data.options.initRotation
+                          : 0;
+          
+          // Our Main DOM Element passed to jQuery
+          data.viewer =  { 
+            width: calcViewerDims('width', $this),
+            height: calcViewerDims('height', $this)
+          };
+          
+          // The Main CONTENTdm Image Properties
+          data.dmImage = {
+            width: data.options.dmImageWidth,
+            height: data.options.dmImageHeight,
+            rotatedWidth: calcDmImageRotatedDimensions('width', $this),
+            rotatedHeight: calcDmImageRotatedDimensions('height', $this)
+          };
+          
+          // The Zoom Image. This is the image that will resize as the user
+          // zooms in and out with slider or controls 
+          // to give a good visual of what zoom level they are are.
+          data.zoomImage = {};
+          
+          // The Minimum Zoom Properties -- These may be updated based on resized
+          // browser, should also be set to the zoom Images default properties
+          data.minZoom = calcMinZoom($this);
+          data.minZoomWidth = calcImageDimensions('width', data.minZoom, $this);
+          data.minZoomHeight = calcImageDimensions('height', data.minZoom, $this);
+          
+          
 
-       });
-     
-     }
+        }
+      });
+    },
+    destroy : function( ) {
+
+      return this.each(function(){
+
+        var $this = $(this),
+            data = $this.data('monocle');
+
+        // Namespacing FTW -- Lets burn this ish down!
+        $(window).unbind('.monocle');
+        data.monocle.remove();
+        $this.removeData('monocle');
+
+      });
+    }
   };
 })(jQuery);
