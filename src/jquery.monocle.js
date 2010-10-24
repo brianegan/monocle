53aaef14a999417f0ea9dcecb5aeca59e8fa4a33
Brian Egan <brian@brianegan.com>
2010-10-24 14:57:13 -0700

    Very basic functionality going -- pulling in a the baseImage.

--- src/jquery.monocle.js
+++ src/jquery.monocle.js
@@ -1,53 +1,186 @@
 /*
  * jQuery Monocle ===========================================================
  *
- * This is the jQuery plugin definition -- it mostly acts as a router, and 
- * utilizes the methods from the Monocle class. 
+ * An image viewer built upon jQueryUI's widget factory, meant to support
+ * multiple formats, such as Zoomify, CONTENTdm, etc
+ *
+ * Terminology:
+ *   - Viewer: The Viewer DOM Element (jQuery Object)
+ *   - sourceImage: The CONTENTdm, Zoomify, etc original source image
+ *   - tileImage: The hi-res Sliced Up Version of the Image sewn together
+ *   - baseImage: The low-res image that zooms in and out for perspective, 
+ *                "replaced" by tileImage once tiles load
+ *   - minImage: The properties of the image at the smallest allowed zoom
+ *	 - current (prefix): The properties of the various images at the user 
+ *                        selected zoom
+ *   - thumbnail: The small version of the image which houses the navigator
+ *   - navigator: The small box contained by the thumbnail which indicates
+ *                current position on the image to the user
  */
-  
-// Stop JavaScript Lint Warnings Messages
-/*jsl:ignoreall*/ 
 
 (function($){
 
-  $.fn.monocle = function(method) {
-    
-    if (Monocle[method]) {
-      return Monocle[method].apply(this, Array.prototype.slice.call(arguments, 1));
-    } else if (typeof method === 'object' || !method) {
-      return Monocle.init.apply(this, arguments);
-    } else {
-      throw 'Method ' +  method + ' does not exist on jQuery.monocle';
-    }    
-  
-  };
-
-  /*
-   * Expose defaults so they can be overridden
-   */
-  $.fn.monocle.defaults = {
-  	thumbMaxWidth: 120,
+$.widget("ui.monocle", {
+	options: {
+		thumbMaxWidth: 120,
     thumbMaxHeight: 120,
     thumbPosition: "TL",
     searchText: "",
     showThumbnail: true,
-    initRotation: 0,
+    rotation: 0,
     dmBridge: true,
     printing: true,
-    urlPrefix: "/",
+    urlPrefix: "",
 		menuPosition: 'before'
-     
-    /* Additional options, not actual defaults
+	},
+	
+	// Create a bunch of empty objects to fill during _creation
+	viewer: {},
+	thumbnail: {},
+	navigator: {},
+
+	// Perform Initial Calculations and Launch the viewer!
+	_create: function() {
+
+		// Cache Viewer Dimensions for Performance
+		this._updateViewerDimensions();
+
+		// Current Image Dimensions
+		this._updateRotatedDimensions();
+
+		// Minimum Zoom
+		this._updateMimimumZoom();
+
+		// Current Image
+		this._updateCurrentImage();
+
+		// Base Image
+		this._appendBaseImage();
+
+	},
+
+	/*
+	 * Appends ==================================================================
+	 *
+	 * We need to inject DOM elements onto the page in a consistent manner
+	 * from a variety of functions
+	 */
+	 _appendBaseImage: function() {
+		var self = this, 
+				baseImage = '<img class="ui-monocle-baseImage" '
+									+ 'alt="Base Image: A Low-Res version for quick viewing" '
+									+ 'width="' + self.options.currentImageWidth + '" '
+									+ 'height="' + this.options.currentImageHeight + '" '
+									+ 'src="'
+									+ this._getImageSrc({
+											width: (self.options.rotation === 0 || self.options.rotation === 180)
+													 ? self.options.minImageWidth
+													 : self.options.minImageHeight,
+											height: (self.options.rotation === 0 || self.options.rotation === 180)
+													  ? self.options.minImageHeight
+													  : self.options.minImageWidth,
+											zoom: self.options.minZoom
+										})
+									+ '" />';
+		
+		this.element.append(baseImage);
+	},
+
+	/*
+	 * Get Image ================================================================
+	 *
+	 * This is a class to build images and tiles from CONTENTdm in a consistent
+	 * way
+	 */
+	_getImageSrc: function(opts) {
+		var zoom = (opts.zoom ? opts.zoom : this.options.zoom) * 100,
+				search = opts.searchText ? opts.searchText : this.options.searchText,
+		    src = this.options.urlPrefix + "/cgi-bin/getimage.exe?"
+						+ "CISOROOT=" + this.options.CISOROOT
+						+ "&CISOPTR=" + this.options.CISOPTR
+						+ "&DMROTATE=" + this.options.rotation
+						+ "&DMSCALE=" + zoom;
+
+		if (opts.width) {
+			src += "&DMWIDTH=" + opts.width;
+		}
+		if (opts.height) {
+			src += "&DMHEIGHT=" + opts.height;
+		}
+		/* if (search) {
+			src += "&DMTEXT=" + search;
+		} */
+
+		return src;	
+	},
+
+	/*
+	 * Setters ====================================================================
+	 *
+	 * Use with calculators to set various dimensions
+	 */
+	_updateViewerDimensions: function() {
+		this.options.viewerWidth = this.element.innerWidth();
+		this.options.viewerHeight = this.element.innerHeight();
+	},
+	_updateMimimumZoom: function(zoom) {
+		this.options.minZoom = zoom ? zoom : this._calcMinZoom();
+		
+		// Need to ensure we set the initial zoom if none exists
+		if (!this.options.zoom) {
+			this.options.zoom = this.options.minZoom;
+		}
+
+		this.options.minImageWidth = this._calcImageDimensions('width', this.options.minZoom);
+		this.options.minImageHeight = this._calcImageDimensions('height', this.options.minZoom);
+	},
+	_updateRotatedDimensions: function(rotation) {
+		var rot = rotation ? rotation : this.options.rotation;
+
+		if (rot === 0 || rot === 180) {
+			this.options.currentSourceImageWidth = this.options.sourceImageWidth;
+			this.options.currentSourceImageHeight = this.options.sourceImageHeight;
+		}	else {
+			this.options.currentSourceImageWidth = this.options.sourceImageHeight;
+			this.options.currentSourceImageHeight = this.options.sourceImageWidth;
+		}
+	},
+	_updateCurrentImage: function() {
+		this.options.currentImageWidth = this._calcImageDimensions('width', this.options.zoom);	
+		this.options.currentImageHeight = this._calcImageDimensions('height', this.options.zoom);	
+	},
+
+	/*
+	 * Calculators ==============================================================
+	 *
+	 */
+	_calcImageDimensions: function(dimension, zoom) {
+		var z = zoom ? zoom : this.options.zoom;
+
+		switch(dimension) {
+			case 'width':
+				return this.options.currentSourceImageWidth * z;
+			case 'height':
+				return this.options.currentSourceImageHeight * z;
+			case 'default':
+				return;
+		}
+	},
+	_calcMinZoom: function() {
+    var minZoomWidthRatio, 
+				minZoomHeightRatio;
     
-    CISOROOT: "/snv",
-    CISOPTR: 2678,
-    dmImageHeight: 3559,
-    dmImageWidth: 5831,
-    initX: 0,
-    initY: 0,
-   
-    */ 
-  };
+    if (this.options.rotation === 0 || this.options.rotation === 180) {
+      minZoomWidthRatio = this.options.viewerWidth / this.options.sourceImageWidth;
+      minZoomHeightRatio = this.options.viewerHeight / this.options.sourceImageHeight;
+    }	else {
+      minZoomWidthRatio = this.options.viewerWidth / this.options.sourceImageHeight;
+      minZoomHeightRatio = this.options.viewerHeight / this.options.sourceImageWidth;
+		}
+
+    return minZoomWidthRatio >= minZoomHeightRatio ? minZoomHeightRatio : minZoomWidthRatio;
+  }
+		
+});
 
 })(jQuery);
-
--- test/unit/monocle_core.js
+++ test/unit/monocle_core.js
@@ -1,158 +1,76 @@
 /*
- * Monocle Tests ====================================================================
+ * Monocle Tests ==============================================================
  *
- * BDD Tests are so sexy. Learn more by Googlin' JasmineBDD
+ * TDD is so sexy. Learn more by Googlin' QUnit and TDD/BDD
  */
 
-describe("monocle", function() {
-  // Create a sandbox div
-  beforeEach(function() {
-    monocle = $('<div />', {
-      id: 'sandbox',
-      css: {
-        width: '900px',
-        height: '400px',
-        padding: '50px'
-      }
-    });
+var el;
 
-    monocle.appendTo('body');
-  });
+(function($) {
 
-  // Reset monocle so it has completely fresh state
-  afterEach(function() { 
-    $('body').remove(monocle);
-    monocle = {};
-  });
+	/* Helper Functions */
+	function opts(option) {
+		return el.monocle('option', option);
+	}
 
+	module("monocle : constructor");
+
+	test("_create", function() {
+		
+		el = $("#monocle").css({
+			width: '900px',
+			height: '400px',
+			padding: '50px'
+		})
+		.monocle({ 
+      CISOROOT: '/zombies',  
+      CISOPTR: 420, 
+      sourceImageHeight: 5000, 
+      sourceImageWidth: 10000, 
+      dmBridge: false,
+      rotation: 90,
+      zoom: 0.3,
+      searchText: "snow",
+      showThumbnail: true
+		});
+
+		// Basic State
+		equals(opts('rotation'), 90, "It should attach the rotation to the data");
+		equals(opts('zoom'), 0.3, "It should attach the zoom to the data");
+
+		// Viewer Calculations
+		equals(opts('viewerWidth'), 1000, "It should calculate and cache the viewer innerWidth()");
+		equals(opts('viewerHeight'), 500, "It should calculate and cache the viwer innerHeight()");
+
+		// Current Rotated sourceImage Dimensions
+		equals(opts('currentSourceImageWidth'), 5000, "It should calculate and cache the current image width (ex: rotated)");
+		equals(opts('currentSourceImageHeight'), 10000, "It should calculate and cache the current image height (ex: rotated)");
+				
+		equals(opts('minZoom'), 0.05, "It should calc and store the minimum zoom level");
+		equals(opts('minImageWidth'), 250, "It should store the mimimum zoom width");
+		equals(opts('minImageHeight'), 500, "It should store the mimimum zoom height");
+		
+		// Current Image Width
+		equals(opts('currentImageWidth'), 1500, "It should set the base image width");
+		equals(opts('currentImageHeight'), 3000, "It should set the base image height");
+
+		// Base Image
+		
+		// Attach the Base Image to the page
+		ok(el.children('.ui-monocle-baseImage').length > 0, "It should append a base image to the page");
+		
+			
+	});
+
+})(jQuery);
+
+/*
   // The plugin is essentially a router to call the appropriate
   // methods. See the official jQuery docs for info.
   describe("jquery plugin", function() {
-    it("should define monocle jquery plugin", function() {
-      expect($.fn.monocle).toBeDefined();
-    });
 
-    it("it should have default options", function() {
-      expect($.fn.monocle.defaults).toBeDefined();
-    });
-    
-    it("should fire the constructor by default", function() {
-      spyOn(Monocle, 'init');
-      monocle.monocle();
-      expect(Monocle.init).toHaveBeenCalled();
-    });
-
-    it("should fire the constructor if an object is passed", function() {
-      spyOn(Monocle, 'init');
-      monocle.monocle({ zoom: 20 });
-      expect(Monocle.init).toHaveBeenCalled();
-    });
-
-    it("should fire the proper method given a method", function() {
-      spyOn(Monocle, 'destroy');
-      monocle.monocle('destroy');
-      expect(Monocle.destroy).toHaveBeenCalled();      
-    });
-    
-    it("should throw an error if a bad method is passed", function() {
-      try { 
-        monocle.monocle('zombies');
-      } catch (e) {
-        expect(e).toEqual('Method zombies does not exist on jQuery.monocle');
-      }
-      // expect(monocle.monocle('zombies')).toThrow('Method zombies does not exist on jQuery.monocle'); 
-    });
-  });  
- 
   // Init Method
   describe("init", function() {
-    beforeEach(function() {
-      monocle.monocle({ 
-        CISOROOT: '/zombies',  
-        CISOPTR: 420, 
-        dmImageHeight: 5000, 
-        dmImageWidth: 10000, 
-        dmBridge: false,
-        rotation: 90,
-        zoom: 0.3,
-        searchText: "snow",
-        urlPrefix: "/",
-        showThumbnail: true
-      });
-      data = monocle.data('monocle');
-    });
-
-    it("should attach data to the element if none exists", function() {
-      expect(data).not.toBeNull();
-    });
-
-    it("should attach the options to the dom element", function() {
-       expect(data.options).not.toBeNull();
-    });
-
-    it("should define the rotation", function() {
-      expect(data.rotation).toEqual(90);
-    });
-
-    it("should define the base zoom", function() {
-      expect(data.zoom).toEqual(0.3);
-    });
-
-    describe("Viewer Calculations", function() {
-      it("should calculate & store the width of the viewer, including padding", function() {
-        expect(data.viewer.width).toEqual(1000);
-      });
-      
-      it("should calculate & store the height of the viewer, including padding", function() {
-         expect(data.viewer.height).toEqual(500);
-      });
-    });  
-
-    describe("CONTENTdm Image Calculations", function() {
-      it("should store the width of the image", function() {
-        expect(data.dmImage.width).toEqual(10000);
-      });
-      
-      it("should store the height of the image", function() {
-        expect(data.dmImage.height).toEqual(5000);
-      });
-
-      it("should store the rotated width of the image", function() {
-        expect(data.dmImage.rotatedWidth).toEqual(5000);
-      });
-      
-      it("should store the rotated height of the image", function() {
-        expect(data.dmImage.rotatedHeight).toEqual(10000);
-      });
-    });  
-    
-    describe("Min Zoom Calculations", function() {
-      it("should store the minimum zoom level", function() {
-        expect(data.minZoom).toEqual(0.1);
-      });
-      
-      it("should store the mimimum zoom width", function() {
-        expect(data.minZoomWidth).toEqual(500); 
-      });
-
-      it("should store the minimum zoom height", function() {
-        expect(data.minZoomHeight).toEqual(1000);
-      });
-    });  
-    
-    describe("Image Calculations", function() {
-      it("should store image data", function() {
-         expect(data.image).toBeDefined();
-      });
-  
-      it("should define the base image width", function() {
-        expect(data.image.width).toEqual(1500);        
-      });
-      
-      it("should define the base image height", function() {
-        expect(data.image.height).toEqual(3000);        
-      });
-    });
 
     describe("Thumbnail Calculations", function() {
       it("should store thumbnail data", function() {
@@ -167,10 +85,10 @@ describe("monocle", function() {
         expect(data.thumbnail.ratio).toEqual(0.012);
       });
       
-      /* it("should set a width", function() {
+      it("should set a width", function() {
         expect(data.thumbnail.width).toEqual(60);
-      }); */
+      }); 
     });  
   });
 });  
-
+ */
